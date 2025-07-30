75d6451dd89de08eb0aa3165d3a0543875cd8928
Aman0221-ak <ak0212singh@gmail.com>
2025-07-31 00:35:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@ app.use(cors({
   }));
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+  app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 
   // API routes
